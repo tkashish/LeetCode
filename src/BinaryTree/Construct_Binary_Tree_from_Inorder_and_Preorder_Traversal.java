73d1cb963cc215ffa99a93bc778165f91a914297
Kashish <ktkashish@gmail.com>
2015-11-02 21:11:05 -0500

    inorder/pretorder BT construction

---
+++ src/BinaryTree/Construct_Binary_Tree_from_Inorder_and_Preorder_Traversal.java
@@ -0,0 +1,15 @@
+package BinaryTree;
+
+public class Construct_Binary_Tree_from_Inorder_and_Preorder_Traversal {
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder.length == 0)return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        int i = 0; 
+        for(i = 0; i < inorder.length; i++){
+            if(inorder[i] == preorder[0])break;
+        }
+        root.left = this.buildTree(Arrays.copyOfRange(preorder,1,1+i), Arrays.copyOfRange(inorder,0,i));
+        root.right = this.buildTree(Arrays.copyOfRange(preorder,1+i,preorder.length), Arrays.copyOfRange(inorder,i+1,inorder.length));
+        return root;
+    }
+}
