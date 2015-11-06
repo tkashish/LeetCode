4eb39d45896c2806adef49e19e9d0208028a474a
Kashish <ktkashish@gmail.com>
2015-11-06 00:23:40 -0500

    LeetCode accepted solution for Best_Time_To_Buy_and_Sell_Stock_III

--- src/DynamicProgramming/Best_Time_To_Buy_and_Sell_Stock_III.java
+++ src/DynamicProgramming/Best_Time_To_Buy_and_Sell_Stock_III.java
@@ -1,63 +1,63 @@
 package DynamicProgramming;
 /*
- * The code below is giving correct answer but TLE for huge entries 
- * runtime  O(n) best case and O(n2) worst case
+ * Accepted solution 
  */
 public class Best_Time_To_Buy_and_Sell_Stock_III {
 	int[] sol = null;
-    int minX = 0;
-    public int maxProfit(int[] prices) {
-        sol = new int[prices.length];
-        int maxProfit = 0;
-        this.findMax(prices, 0, prices.length-1, true);
-        maxProfit = sol[prices.length-1];
-        for(int i = 0; i < prices.length; i++){
-            System.out.print(sol[i] + " ");
-        }
-        System.out.println(minX);
-        for(int i = 1; i < prices.length; i++){
-            int profit = sol[i-1];
-            int p1 = 0;
-            if(i < minX){
-                p1 += sol[prices.length-1];
-            }else{
-                p1 += this.findMax(prices, i, prices.length-1, false); 
-            }
-            System.out.println(p1 +"  " + sol[i-1] + " " + i);
-            profit += p1;
-            if(profit > maxProfit) maxProfit = profit;
-        }
-        return maxProfit;
-    }
-    public int findMax(int[] prices, int i, int j, boolean flag){
-        if(j < i) return 0;
-        int Max = i;
-        int Min = i;
-        int profit = 0;
-        if(flag){
-        	this.sol[i] = 0;
-        }
-        for(int k = i+1; k < j+1; k++){
-            if(prices[k] < prices[Min]) {
-                Min = k;
-                Max = k;
-            }
-            if(prices[k] > prices[Max]) Max = k;
-            if(prices[Max]-prices[Min] > 0){
-                profit=prices[Max]-prices[Min];
-                if(flag){
-                	this.minX = Min;             
-                }
-            }
-            if(flag){
-            	this.sol[k] = profit;
-            }
-        }
-        return profit;
-    }
-    public static void main(String[] args){
-    	int[] prices = {1,-2,3,1,-6,2,7,-1,-2,9};
-    	Best_Time_To_Buy_and_Sell_Stock_III t = new Best_Time_To_Buy_and_Sell_Stock_III();
-    	t.maxProfit(prices);
-    }
+	int[] solNext = null;
+	public int maxProfit(int[] prices) {
+		if(prices.length == 0) return 0;
+		sol = new int[prices.length];
+		int maxProfit = 0;
+		solNext = new int[prices.length];
+		this.findMax(prices);
+		this.findMaxNext(prices);
+		maxProfit = sol[prices.length-1];
+		for(int i = 1; i < prices.length; i++){
+			int profit = sol[i-1] + solNext[i];
+			if(profit > maxProfit) maxProfit = profit;
+		}
+		return maxProfit;
+	}
+	public int findMaxNext(int[] price){
+		int min = price.length-1;
+		int max = price.length-1;
+		int profit = 0;
+		solNext[price.length-1] = 0;
+		for(int i = price.length-2; i >= 0; i--){
+			if(price[i] < price[min]){
+				min = i;
+			}
+			if(price[i] > price[max]){
+				min = i;
+				max = i;
+			}
+			if(profit < price[max] - price[min]) profit =  price[max] - price[min];
+			solNext[i] = profit;
+		}
+		return profit;
+	}
+	public int findMax(int[] prices){
+		int Max = 0;
+		int Min = 0;
+		int profit = 0;
+		this.sol[0] = 0;
+		for(int k = 1; k < prices.length; k++){
+			if(prices[k] < prices[Min]) {
+				Min = k;
+				Max = k;
+			}
+			if(prices[k] > prices[Max]) Max = k;
+			if(prices[Max]-prices[Min] > 0){
+				profit=prices[Max]-prices[Min];
+			}
+			this.sol[k] = profit;
+		}
+		return profit;
+	}
+	public static void main(String[] args){
+		int[] prices = {1,-2,3,1,-6,2,7,-1,-2,9};
+		Best_Time_To_Buy_and_Sell_Stock_III t = new Best_Time_To_Buy_and_Sell_Stock_III();
+		System.out.println(t.maxProfit(prices));
+	}
 }
