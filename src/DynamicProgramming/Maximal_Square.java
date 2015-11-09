3d3dc78aa95a586811d1b1abb583b4b294e43b2a
Kashish <ktkashish@gmail.com>
2015-11-09 18:11:41 -0500

    Maximal Square

---
+++ src/DynamicProgramming/Maximal_Square.java
@@ -0,0 +1,55 @@
+package DynamicProgramming;
+
+public class Maximal_Square {
+	public static int maximalSquare(char[][] matrix) {
+		int max = 0;
+		for(int i = 0; i < matrix.length; i++){
+			for(int j = 0; j < matrix[0].length; j++){
+				if(i != 0 && j != 0){
+					if(matrix[i][j] != '0'){
+						int i1 = (int)matrix[i-1][j] - 48;
+						int i2 = (int)matrix[i-1][j-1] - 48;
+						int i3 = (int)matrix[i][j-1] - 48;
+						matrix[i][j] = Integer.toString((1 + Math.min(i1, Math.min(i2, i3)))).charAt(0);
+					}
+				}
+				max = Math.max(max, ((int)matrix[i][j])-48);
+			}
+		}
+		return max*max;
+	}
+	public static int maximalSq(char[][] matrix){
+        if(matrix.length == 0) return 0;
+		int[][] table = new int[matrix.length][matrix[0].length];
+		int max = 0;
+		for(int i = 0; i < matrix.length; i++){
+			for(int j = 0; j < matrix[0].length; j++){
+				if(i == 0 || j == 0){
+					if(matrix[i][j] == '0') table[i][j] = 0;
+					else table[i][j] = 1;
+				}else{
+					if(matrix[i][j] == '1'){
+						table[i][j] = 1+Math.min(table[i-1][j], Math.min(table[i][j-1],table[i-1][j-1]));
+					}
+					else table[i][j] = 0;
+				}
+				max = Math.max(max, table[i][j]);
+			}
+		}
+		return max*max;
+	}
+	public static void print(char[][] matrix){
+		for(int i = 0; i < matrix.length; i++){
+			for(int j = 0; j < matrix[0].length; j++){
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args){
+		char[][] input = {{'1','0','1','0','0'}, {'1','0','1','1','1'}, {'1','1','1','1','1'}, {'1','0','0','1','1'}};
+		print(input);
+//		System.out.println(maximalSquare(input));
+		System.out.println(maximalSq(input));
+	}	
+}
--- src/DynamicProgramming/MaximumProductSubArray.java
+++ src/DynamicProgramming/MaximumProductSubArray.java
@@ -35,6 +35,8 @@ public class MaximumProductSubArray {
 	public static void main(String[] args){
 		int[] input = {2,3,-4,110};
 		System.out.println(maxProduct(input));
-		
+		int i = 0;
+		char j = '1';
+		System.out.println((int) ((char) i - 110));
 	}
 }
